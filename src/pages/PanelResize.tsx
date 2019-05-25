784df72e74c5dfbdcc1412774fa085bd40f6f409
Thomas <tom@axisj.com>
2019-05-26 01:21:04 +0900

    ..

--- src/pages/PanelResize.tsx
+++ src/pages/PanelResize.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import styled from "styled-components";
 import { SMixinFlexRow, SMixinFlexColumn } from "../styled/mixins";
-import mouseEventSubscribe from "../util/mouseEventSubscribe";
+// import mouseEventSubscribe from "../util/mouseEventSubscribe";
 
 const Container = styled.div`
-  height: 100vh;
+
+
+  /* height: 100vh;
   ${SMixinFlexRow("stretch", "stretch")};
 
   [data-panel] {
@@ -27,75 +29,75 @@ const Container = styled.div`
       height: 100%;
       left: -4px;
       top: 0;
-      /* background: rgba(255, 0, 0, 0.3); */
+      background: rgba(255, 0, 0, 0.3);
       cursor: ew-resize;
     }
 
     &.resizing {
     }
-  }
+  } */
 `;
 
 interface IProps {}
-interface IState {
-  panelAGrow: number;
-  panelBGrow: number;
-  resizing: boolean;
-}
+// interface IState {
+//   panelAGrow: number;
+//   panelBGrow: number;
+//   resizing: boolean;
+// }
 
-class PanelResize extends React.Component<IProps, IState> {
-  containerRef: React.RefObject<HTMLDivElement>;
+class PanelResize extends React.Component<IProps> {
+  // containerRef: React.RefObject<HTMLDivElement>;
 
-  state = {
-    panelAGrow: 1,
-    panelBGrow: 2,
-    resizing: false
-  };
+  // state = {
+  //   panelAGrow: 1,
+  //   panelBGrow: 2,
+  //   resizing: false
+  // };
 
-  constructor(props: IProps) {
-    super(props);
-    this.containerRef = React.createRef();
-  }
+  // constructor(props: IProps) {
+  //   super(props);
+  //   this.containerRef = React.createRef();
+  // }
 
-  handleResizerMove = (e: React.MouseEvent) => {
-    if (!this.containerRef.current) {
-      return;
-    }
+  // handleResizerMove = (e: React.MouseEvent) => {
+  //   if (!this.containerRef.current) {
+  //     return;
+  //   }
 
-    const {
-      left: containerLeft
-    } = this.containerRef.current.getBoundingClientRect();
+  //   const {
+  //     left: containerLeft
+  //   } = this.containerRef.current.getBoundingClientRect();
 
-    const panelA = this.containerRef.current.querySelector('[data-panel="A"]');
-    const panelB = this.containerRef.current.querySelector('[data-panel="B"]');
-    if (!panelA || !panelB) {
-      return;
-    }
-    const panelsWidth = panelA.clientWidth + panelB.clientWidth + 1;
+  //   const panelA = this.containerRef.current.querySelector('[data-panel="A"]');
+  //   const panelB = this.containerRef.current.querySelector('[data-panel="B"]');
+  //   if (!panelA || !panelB) {
+  //     return;
+  //   }
+  //   const panelsWidth = panelA.clientWidth + panelB.clientWidth + 1;
 
-    mouseEventSubscribe(
-      mpos => {
-        // console.log(
-        //   panelsWidth,
-        //   mpos.clientX - containerLeft,
-        // );
+  //   mouseEventSubscribe(
+  //     mpos => {
+  //       // console.log(
+  //       //   panelsWidth,
+  //       //   mpos.clientX - containerLeft,
+  //       // );
 
-        const panelAGrow = ((mpos.clientX - containerLeft) / panelsWidth) * 2;
-        const panelBGrow = 2 - panelAGrow;
-        this.setState({
-          panelAGrow,
-          panelBGrow
-        });
-      },
-      () => {}
-    );
-  };
+  //       const panelAGrow = ((mpos.clientX - containerLeft) / panelsWidth) * 2;
+  //       const panelBGrow = 2 - panelAGrow;
+  //       this.setState({
+  //         panelAGrow,
+  //         panelBGrow
+  //       });
+  //     },
+  //     () => {}
+  //   );
+  // };
 
   render() {
-    const { panelAGrow, panelBGrow, resizing } = this.state;
+    // const { panelAGrow, panelBGrow, resizing } = this.state;
     return (
-      <Container ref={this.containerRef}>
-        <div data-panel="A" style={{ flexGrow: panelAGrow }}>
+      <Container>
+        {/* <div data-panel="A" style={{ flexGrow: panelAGrow }}>
           A<p>({panelAGrow})</p>
         </div>
         <div
@@ -106,7 +108,7 @@ class PanelResize extends React.Component<IProps, IState> {
         />
         <div data-panel="B" style={{ flexGrow: panelBGrow }}>
           B<p>({panelBGrow})</p>
-        </div>
+        </div> */}
       </Container>
     );
   }
