08de4a1c2fd5806983e5f2ee048f397f90d2be76
Thomas <tom@axisj.com>
2019-05-26 16:34:49 +0900

    dragdrop

--- src/pages/DragDrop.tsx
+++ src/pages/DragDrop.tsx
@@ -2,22 +2,132 @@ import React from "react";
 import styled from "styled-components";
 
 const Container = styled.div`
-  margin: 30px;
+  margin: 30px auto;
   width: 500px;
-  padding: 5px 0 0;
-  background: #eee;
 `;
 
+const Panel = styled.div``;
+
 interface IProps {}
-interface IState {}
-class DragDrop extends React.Component<IProps, IState> {
+class DragDrop extends React.Component<IProps> {
   render() {
     return (
       <Container>
-        <div data-tab="0">Label 0</div>
+        <Panel>Panel</Panel>
       </Container>
     );
   }
 }
 
 export default DragDrop;
+
+// const Panel = styled.div`
+//   background: #eee;
+//   border: 2px solid #666;
+//   border-radius: 5px;
+//   padding: 10px;
+//   text-align: center;
+//   margin: 10px 0;
+//   cursor: move;
+
+//   &[data-draged="true"] {
+//     opacity: 0.4;
+//   }
+// `;
+
+// interface IPanel {
+//   id: string;
+//   label: string;
+// }
+// interface IProps {}
+// interface IState {
+//   panels: IPanel[];
+//   dragingPanelIndex?: number;
+//   dragoverPanelIndex?: number;
+//   dragWhereOver?: "before" | "after";
+// }
+// class DragDrop extends React.Component<IProps, IState> {
+//   constructor(props: IProps) {
+//     super(props);
+
+//     this.state = {
+//       panels: [
+//         { id: "A", label: "panel A" },
+//         { id: "B", label: "panel B" },
+//         { id: "C", label: "panel C" },
+//         { id: "D", label: "panel D" }
+//       ]
+//     };
+//   }
+
+//   handleDragStart = (dragingPanelIndex: number) => {
+//     this.setState({
+//       dragingPanelIndex
+//     });
+//   };
+//   handleDragOver = (e: React.DragEvent<HTMLDivElement>) => {
+//     const { dragingPanelIndex } = this.state;
+//     const dragoverPanelIndex = Number(
+//       e.currentTarget.getAttribute("data-index")
+//     );
+
+//     if (dragoverPanelIndex === dragingPanelIndex) {
+//       return;
+//     }
+
+//     const { top, height } = e.currentTarget.getBoundingClientRect();
+//     const dragWhereOver = top + height / 2 > e.clientY ? "before" : "after";
+
+//     this.setState({
+//       dragoverPanelIndex,
+//       dragWhereOver
+//     });
+//   };
+//   handleDragEnd = () => {
+//     //
+
+//     this.setState({
+//       dragingPanelIndex: undefined,
+//       dragoverPanelIndex: undefined,
+//       dragWhereOver: undefined
+//     });
+//   };
+
+//   render() {
+//     const {
+//       panels,
+//       dragingPanelIndex,
+//       dragoverPanelIndex,
+//       dragWhereOver
+//     } = this.state;
+//     return (
+//       <Container>
+//         {panels.map((p, i) => {
+//           return (
+//             <React.Fragment key={i}>
+//               {dragoverPanelIndex === i && dragWhereOver === "before" && (
+//                 <div>Drop here</div>
+//               )}
+//               <Panel
+//                 data-index={i}
+//                 data-draged={i === dragingPanelIndex}
+//                 key={i}
+//                 draggable={true}
+//                 onDragStart={() => this.handleDragStart(i)}
+//                 onDragEnd={this.handleDragEnd}
+//                 onDragOver={this.handleDragOver}
+//               >
+//                 {p.label}
+//               </Panel>
+//               {dragoverPanelIndex === i && dragWhereOver === "after" && (
+//                 <div>Drop here</div>
+//               )}
+//             </React.Fragment>
+//           );
+//         })}
+//       </Container>
+//     );
+//   }
+// }
+
+// export default DragDrop;
