7522553848a67fd599a16003041c2861e45e9ee9
Thomas <tom@axisj.com>
2019-05-27 20:36:42 +0900

    add prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import './test.less';
+import MarkdownView from './markdown/Markdown';
+import './styles/index';
 
 const App: React.FC = () => {
   return (
-    <div style={{ padding: "0 20px" }}>
+    <div style={{ padding: '0 20px' }}>
       <MarkdownView />
     </div>
   );
